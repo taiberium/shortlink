9a8425da1baab3f7ac5ffe30e5af4189b36212ef
taiberium <taiberium@mail.ru>
2020-05-22 17:37:59 +0300

    init

---
+++ src/test/java/com/shortlink/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortlink.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
